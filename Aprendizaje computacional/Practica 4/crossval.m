eabeb38f4feb12ecedd446615dc95806f33d8530
glidder <luisjquintana@gmail.com>
2014-05-13 18:01:16 +0200

    Fumasa is back

--- Aprendizaje computacional/Practica 4/crossval.m
+++ Aprendizaje computacional/Practica 4/crossval.m
@@ -1,17 +1,11 @@
 function [datatrn,dataval,outtrn,outval]=crossval(data,out,N,i);
-% CROSSVAL - Extrae el sunbconjunto de patrones i-esimo de los N posibles
-%
-%      [datatrn,dataval,outtrn,outval]=crossval(data,out,N,i);
-
-%
-%	Copyright (c) Pedro L. Galindo (1998)
 
 umbrales = round(linspace(1,size(data,1)+1,N+1));
 inicios = umbrales(1:N);
 finales = umbrales(2:N+1)-1;
 
-datatrn = [data(1:inicios(i)-1,:); data(finales(i)+1:size(data,2),:)];
-outtrn  = [out(1:inicios(i)-1,:);  out(finales(i)+1:size(data,2),:)];
+datatrn = [data(1:inicios(i)-1,:); data(finales(i)+1:size(data,1),:)];
+outtrn  = [out(1:inicios(i)-1,:);  out(finales(i)+1:size(data,1),:)];
 
 dataval = data(inicios(i):finales(i),:);
 outval  = out(inicios(i):finales(i),:);
