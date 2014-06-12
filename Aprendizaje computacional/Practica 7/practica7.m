44d19edd69c4564b5eb4a7306cd810a109b57e35
glidder <luisjquintana@gmail.com>
2014-06-13 00:41:58 +0200

    I see you

--- Aprendizaje computacional/Practica 7/practica7.m
+++ Aprendizaje computacional/Practica 7/practica7.m
@@ -7,3 +7,9 @@ figure,
 subplot(1,3,1),dendrogram(jerarquico(datos,'sigle'));title('Single Link')
 subplot(1,3,2),dendrogram(jerarquico(datos,'complete'));title('Complete Link')
 subplot(1,3,3),dendrogram(jerarquico(datos,'group'));title('Group Average Link')
+
+load cities.mat
+figure,
+subplot(1,3,1),dendrogram(jerarquico(ratings,'sigle'));title('Single Link')
+subplot(1,3,2),dendrogram(jerarquico(ratings,'complete'));title('Complete Link')
+subplot(1,3,3),dendrogram(jerarquico(ratings,'group'));title('Group Average Link')
